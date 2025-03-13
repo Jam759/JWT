782decbbf6b194b58daebe4a88de1bc472deb030
Jam759 <jam759@naver.com>
2025-03-14 00:40:25 +0900

    예외처리 변경

--- src/main/java/com/example/clubs/member/exception/UserNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.example.clubs.member.exception;
-
-public class UserNotFoundException extends RuntimeException {
-    public UserNotFoundException(String message) {
-        super(message);
-    }
-}
